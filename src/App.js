83c4257d6eaa28063f25a6cfd3f1c9e1741d98ff
Brittany <brittanylcallus@gmail.com>
2022-10-30 12:43:54 -0500

    basic navbar routes mapped out

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 const App = () => {
 
     return (
-        <Navbar />
+        <div>
+            <Navbar />
+            {/* <Routes>
+                <Route exact path='/' element={<Home />}></Route>
+                <Route path='about' element={<About />}></Route>
+                <Route path='projects' element={<Projects />}></Route>
+                <Route path='skills' element={<Skills />}></Route>
+            </Routes> */}
+        </div>
     )
 }
 
