60a4461f267171b46fdc035c88b34f9ff75c6745
Brittany <brittanylcallus@gmail.com>
2022-10-30 12:21:37 -0500

    navbar flex added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const Navbar = () => {
 
     return (
-        <h1>Navbar Test</h1>
+        <div className='navbar'>
+            <h1>BC.</h1>
+            <ul className='navbar-tabs'>
+                <h2>Home</h2>
+                <h2>About</h2>
+                <h2>Projects</h2>
+                <h2>Skills</h2>
+            </ul>
+        </div>
     )
 }
 
