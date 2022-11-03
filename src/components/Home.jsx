39502cace73cec7f707616b186cdcebea3c4c335
Brittany <brittanylcallus@gmail.com>
2022-11-03 12:54:51 -0500

    basic home page draft text added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,9 @@ const Home = () => {
     return (
         <div>
             <h1>Home Test</h1>
+            <h1>Hello</h1>
+            <p>I'm Brittany Callus</p>
+            <p>the full stack developer - the sales manager - the musician</p>
             <img src={headshot} alt='headshot' className='headshot'/>
         </div>
         
