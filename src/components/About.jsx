6aee6217c388dc8f48665e737761b542bd43686d
Brittany <brittanylcallus@gmail.com>
2022-11-08 17:01:16 -0600

    about me section started

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const About = () => {
 
     return (
-        <h1>About Test</h1>
+        <div>
+            <h1>Who am I?</h1>
+            <p>{`I am a recent graduate of the University of North Florida's web development program which is powered by Fullstack Academy. I have proficiencies and experience in developing full stack web applications using HTML, CSS, JavaScript, React JS, Node JS, Express JS, and PostgreSQL. See Skills a more detailed list.`}</p>
+            <p>{`I have been a studio manager with a focus in sales for the past 5 years in the fitness/health & wellness industry. Prior to that I was an administrative assistant for a company that specialized in forensic data analysis, and that’s where I began building my full stack development skills.`}</p>
+            {/* //perhaps create a mission statement page */}
+            <p>{`I believe that my web development skillset and previous business experience will make me an asset to the right engineering team. My ability to effectively analyze and affect business needs (i.e. facilitating growth, monthly goals, team building, etc.) coupled with my self-discipline and short/long term goal setting strengths will enhance my growing development skills, making me a strong employee. I really do enjoy setting and achieving attainable goals and I am always striving to learn more. It’s something I try to implement daily even in my personal life.`}</p>
+        </div>
     )
 
 }
