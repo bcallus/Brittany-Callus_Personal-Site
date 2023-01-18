5cfa933cdb39cf49df1386d1ce1bb3e16ca7081e
Brittany <brittanylcallus@gmail.com>
2023-01-18 17:40:59 -0600

    project card outline initialized

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 const Projects = () => {
 
     return (
-        <h1>Projects Test</h1>
+        <div>
+            <div className="project-card-container">
+                {/* map through projects list */}
+                <div> {/*needs unique key*/}
+                    <p>Project Name</p>
+                    <p>Project Image - use img tag</p>
+                    <p>Project Description</p>
+                    <button>View Project</button>
+                </div>
+            </div>
+
+        </div>
     )
 
 }
