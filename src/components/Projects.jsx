23476a1f9b13a96b721c5a092ac79db04a71c855
Brittany <brittanylcallus@gmail.com>
2023-01-31 13:30:55 -0600

    project dummy data mapped

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useEffect }  from 'react';
+import projectsData from '../data/projectsData.js'
 
 const Projects = () => {
+    const projectsList = projectsData;
+    // useEffect(() => {
+    //     console.log("projectsList", projectsList)
+    // }, [])
 
     return (
         <div>
             <div className="project-card-container">
-                {/* map through projects list */}
-                <div> {/*needs unique key*/}
-                    <p>Project Name</p>
-                    <p>Project Image - use img tag</p>
-                    <p>Project Description</p>
-                    <button>View Project</button>
+                {projectsList.map((project) => (
+                    <div key={project.id}>
+                        <p>{project.name}</p>
+                        <p>Project Image - use img tag</p>
+                        <p>{project.description}</p>
+                        <button>View Project</button>
                 </div>
+                ))}
             </div>
 
         </div>
