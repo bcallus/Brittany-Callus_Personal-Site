495b7d1772bc274c3a39cfeb4d94e1bbabc6d8a8
Brittany <brittanylcallus@gmail.com>
2023-02-02 12:15:21 -0600

    project data added

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,6 +1,6 @@
-import CoffeeTea_Project from '../assets/CoffeeTea_Project.png';
-import StrangersThings_Project from '../assets/StrangersThings_Project.png';
-import TheArcade_Project from '../assets/TheArcade_Project.png';
+import CoffeeTea_Project from "../assets/CoffeeTea_Project.png";
+import StrangersThings_Project from "../assets/StrangersThings_Project.png";
+import TheArcade_Project from "../assets/TheArcade_Project.png";
 
 const projectData = [
   {
@@ -10,19 +10,25 @@ const projectData = [
     description:
       "Coffee & Tea is a basic e-commerce web application that allows any user to browse and/or purchase from a variety of high quality coffee and teas. Additional capabilities open up to registered users, and site maintenance capabilities do exist but are restricted to administrator access only.",
     techStack:
-      "Languages & Technologies Used: HTML, CSS, Javascript, Node, NPM, SQL, PostgreSQL, React JS, Express JS, Git, GitHub Projects, Slack, Zoom, Discord",
+      "Languages & Technologies Used: HTML, CSS, Javascript, Node, NPM, SQL, PostgreSQL, React JS, Express JS, Git, GitHub Projects, Slack, Zoom, Discord"
   },
   {
     id: 2,
     name: "Stranger's Things",
     img: StrangersThings_Project,
-    description: "This is a brief description of Project Two.",
+    description:
+      '"Craigslist-lite” style front end application designed to allow a user to navigate through posts and corresponding messages.',
+    techStack:
+      "Languages & Technologies Used: HTML, CSS, Javascript, AJAX, React JS, GitHub, Netlify."
   },
   {
     id: 3,
     name: "The Arcade",
     img: TheArcade_Project,
-    description: "This is a brief description of Project Two.",
+    description:
+      "Classic arcade came “Snake” where the perpetually moving snake grows and becomes faster upon eating apples but achieves game over if it hits or runs into something else.",
+    techStack:
+      "Languages & Technologies Used: HTML, CSS, Javascript, DOM manipulation, GitHub, Netlify.",
   },
 ];
 
