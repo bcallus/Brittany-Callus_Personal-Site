7d07ad9578dd795a6ad85c1cfe854df067300ef3
Brittany <brittanylcallus@gmail.com>
2023-02-08 08:41:09 -0600

    skill data added

--- src/data/skillsdata.js
+++ src/data/skillsdata.js
@@ -3,6 +3,7 @@ import CSS_logo from '../assets/CSS_logo.png'
 import Javascript_logo from '../assets/Javascript_logo.png'
 import REST_image from '../assets/REST_image.png'
 import React_logo from '../assets/React_logo.png'
+import Node_logo from '../assets/Node_logo.png'
 
 
 const skillsData = [
@@ -30,6 +31,11 @@ const skillsData = [
         id: 5,
         name: 'React JS',
         img: React_logo
+    },
+    {
+        id: 6,
+        name: 'Node JS',
+        img: Node_logo
     },
 ]
 
